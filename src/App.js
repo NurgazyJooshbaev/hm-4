acc6d26f5ac0b24031a6cd55cf273fb49cd25422
Nurgazy <tagaevnurgazy.kg@gmail.com>
2022-10-14 18:06:21 +0300

    added new text

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      HM-4 
+      <p>
+         Welcome to GitHub
+      </p>
+
+      <p>New Project</p>
+
+    </div>
+  );
+}
+
+export default App;
